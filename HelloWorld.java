9f90565b85f11070ddcab227605653df307cc74a
tariky-campos <tariky.campos@ufv.br>
2025-09-03 16:59:49 -0300

    feat: Criacao da classe HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int resultado = somar(5, 10);
+        System.out.println("O resultado da soma é: " + resultado);  
+    }
+
+
+    public static int somar(int a, int b) {
+        return a + b;
     }
 }
