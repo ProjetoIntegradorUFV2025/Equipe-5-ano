2f9d918d75d5c6573b9ce2fab3b598c4c70560e1
tariky-campos <tariky.campos@ufv.br>
2025-09-24 18:06:07 -0300

    feat: Criação do banco ByteCraft e tabela alunos com dados de teste

---
+++ bytecraft.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE ByteCraft;
+USE ByteCraft;
+CREATE TABLE alunos (
+	apelido VARCHAR(45) PRIMARY KEY,
+    sala SMALLINT
+);
+INSERT INTO alunos (apelido, sala) VALUES
+('Tariky', 5),
+('Tonao',11);
