e3319077d514b919a8384f467618fa164ee676d0
João Pedro <jotinha156@gmail.com>
2025-09-25 18:01:40 -0300

    feat: Criação da classe Professor

---
+++ Professor.java
@@ -0,0 +1,39 @@
+public class Professor {
+
+  public String nomeDeUsuario;
+  private String senha;
+  public  Sala sala;
+
+  
+    public String getNomeDeUsuario() {
+        return nomeDeUsuario;
+    }
+
+    public void setNomeDeUsuario(String nomeDeUsuario) {
+        this.nomeDeUsuario = nomeDeUsuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Sala getSala() {
+        return sala;
+    }
+
+    public void setSala(Sala sala) {
+        this.sala = sala;
+
+        
+        }
+          
+
+    }
+
+   
+
+    
