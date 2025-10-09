46925735929589ce7e9d6bd21b947598e7f601a3
Pablo <pablosantana691@gmail.com>
2025-10-09 08:35:59 -0300

    feat: Adicionado o codigo da classe Peca

---
+++ Peca.java
@@ -0,0 +1,26 @@
+public class Peca {
+    private int id;
+    private String nome;
+
+    //Método Construtor
+    public Peca(int id, String nome){
+        this.id = id;
+        this.nome = nome;
+    }
+
+    //Métodos Getters
+    public int getId(){
+        return this.id;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+
+    //Métodos Setters
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+}
