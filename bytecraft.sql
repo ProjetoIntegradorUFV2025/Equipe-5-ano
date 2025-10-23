f6d6060bb4d28ad71b150e70b487acd255d92ec3
Pedro Paulo <pedro.p.nascimento13@gmail.com>
2025-10-23 15:32:19 -0300

    feat: Consulta em ordem Alfabética

--- bytecraft.sql
+++ bytecraft.sql
@@ -13,4 +13,14 @@ INSERT INTO alunos (apelido, sala, pontuacao) VALUES
 ('Brunao', '99', 9),
 ('Pedro', '103', 8),
 ('Pablo', '27', 10),
-('Nery', '3', 7);
+('Nery', '11', 2),
+('Carlos', '103', 10),
+('Abner', '69', 9),
+('Luiz', '44', 6),
+('Mauro', '99', 5),
+('Aline', '11', 5),
+('Paulo', '101', 4);
+
+SELECT apelido 
+FROM alunos
+ORDER BY apelido ASC;
