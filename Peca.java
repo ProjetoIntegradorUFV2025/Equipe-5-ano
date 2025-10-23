09f9bce281ae76fde8eeb005212a37920ff184b6
tariky-campos <tariky.campos@ufv.br>
2025-10-23 19:51:45 -0300

    feat: Herança e Polimorfismo

--- Peca.java
+++ Peca.java
@@ -1,25 +1,30 @@
-public class Peca{
+public class Peca {
     private int id;
     private String nome;
 
+    //Método Construtor
     public Peca(int id, String nome){
         this.id = id;
         this.nome = nome;
     }
 
-    //Metodos Getters
+    //Métodos Getters
     public int getId(){
-        return id;
+        return this.id;
     }
     public String getNome(){
-        return nome;
+        return this.nome;
     }
 
-    //Metodos setters
+    //Métodos Setters
     public void setId(int id){
         this.id = id;
     }
     public void setNome(String nome){
         this.nome = nome;
     }
+
+    public void exibirDetalhes(){ 
+        System.out.println("Peça: " + nome + " (ID: " + id + ")"); 
+    } 
 }
