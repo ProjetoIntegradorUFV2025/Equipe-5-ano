40145fe6066b261b683e9f836e9f86d86037d287
tariky-campos <tariky.campos@ufv.br>
2025-10-30 23:01:54 -0300

    feat: Buscando Perguntas no banco de dados

---
+++ bytecraft.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE bytecraft; 
+
+USE bytecraft;
+
+CREATE TABLE pergunta (
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    enunciado VARCHAR(255), 
+    alternativaA VARCHAR(255),
+    alternativaB VARCHAR(255), 
+    alternativaC VARCHAR(255), 
+    alternativaD VARCHAR(255), 
+    correta CHAR(1), 
+    nivelDificuldade ENUM('FACIL', 'MEDIO', 'DIFICIL') 
+);
+
+INSERT INTO pergunta (enunciado, alternativaA, alternativaB, alternativaC, alternativaD, correta, nivelDificuldade)
+VALUES
+    ('Qual é a capital do Brasil?', 'São Paulo', 'Rio de Janeiro', 'Brasilia', 'Salvador', 'C', 'FACIL'),
+    ('Quanto é 5+6?', '10', '20', '30', '11', 'D', 'FACIL'), 
+    ('Quem escreveu Dom Casmurro?', 'Machado de Assis', 'José de Alencar', 'Clarice Lispector', 'Monteiro Lobato', 'A', 'MEDIO');
+
+
+SELECT P.*
+FROM pergunta AS P
+WHERE 
+    nivelDificuldade = 'MEDIO';
